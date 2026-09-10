7aafcddd2c218190d321a78859802e2d532464bf
Faldi Harido Fihan <80746946+Falllll@users.noreply.github.com>
2026-09-10 17:38:45 +0700

    feat(api): document the api contract as an openapi 3.0 spec (#19)

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,13 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use Symfony\Component\HttpFoundation\Response;
+
+Route::get('/openapi.yaml', function (): Response {
+    return response()->file(base_path('docs/api/openapi.yaml'), [
+        'Content-Type' => 'application/yaml',
+    ]);
+})->name('openapi.spec');
 
 Route::get('/user', function (Request $request) {
     return $request->user();
